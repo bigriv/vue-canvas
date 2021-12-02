631d223af23ff43bf932536f0aaf7325dc5c41ba
PandemiccerYoshiwo <pandemiccer@gmail.com>
2021-12-02 22:22:11 +0900

    add: start project - vue3 - vuetify3α - vue-router - vuex

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Vuetify
+import { createVuetify } from "vuetify";
+
+export default createVuetify({
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  defaults: {
+    global: {
+      ripple: false,
+    },
+    VSheet: {
+      elevation: 4,
+    },
+  },
+});
